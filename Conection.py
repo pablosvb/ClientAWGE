0374326f29eb71afa421eafe67dd355b309f5100
pablosvb1 <pablosvb1@gmail.com>
2023-10-26 14:19:10 +0200

    s

---
+++ Conection.py
@@ -0,0 +1,73 @@
+import socket
+from src.application.Logger import Logger
+from src.application.view import View
+
+logger = Logger()
+
+
+class Conection:
+
+    def __init__(self):
+        View.clear()
+        View.cabecera()
+        self.mensaje = ""
+        self.host = input("[38;5;20m Introduce el Host:[92;5;154m")
+        self.port = int (input("[38;5;20m Introduce el Puerto:[92;5;154m"))
+        self.conexion()
+    
+  
+    
+    def conexion(self):
+        
+        def escuchar_servidor(sock):
+            self.mensaje = input("[38;5;20m Introduzca el Comando , help para visualizar las opciones o 'salir' para terminar):[92;5;154m ")
+            s.sendall(self.mensaje.encode())
+            while True:
+                data = sock.recv(1024)
+                if data:
+                    print(f'[38;5;33m Server: [92;5;92m{data.decode()}')
+                    self.mensaje = input("[38;5;20m Introduzca el Comando , help para visualizar las opciones o 'salir' para terminar):[92;5;154m ")
+                    if self.mensaje.lower() == 'salir':
+                        s.close()
+                        Conection()
+                        break
+                    s.sendall(self.mensaje.encode())
+
+                
+
+        try:
+            error = False
+            logger.debug("Conectando...")
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.host, self.port))
+            logger.debug(f'Conectado al servidor: {self.host}:{self.port}')
+            View.clear()
+            View.cabecera()
+            escuchar_servidor(s)
+                    
+                
+
+        except socket.gaierror as e:
+            error = True
+            if e.errno == 11001:
+                logger.debug('ERROR 11001: Los Datos de HOST Y PUERTO introducidos no son correctos')
+            else:
+                logger.debug(f'Error gaierror: {e}')
+        except socket.timeout as e:
+            error = True
+            if e.errno == 10060:
+                logger.debug('ERROR 10060: Se ha agotado el tiempo de espera para la conexión. El servidor no ha respondido a tiempo., puede que introduzca mal el HOST u/o el PORT')
+            elif e.errno == 10061:
+                logger.debug("ERROR 10061: No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión, puede que introduzca mal el HOST u/o el PORT")
+        except Exception as e:  
+            s.close()
+            error = True
+            logger.debug(f'ERROR: {e}')
+        finally:
+            try:
+                if (error == True):
+                    Conection()
+            except:
+                pass
+    
+Conection()
---
+++ src/application/view.py
@@ -0,0 +1,21 @@
+from src.domain.cabecera import __header_AWGE__, __header_W__, __header_PMC__, __header_sxplain__, __header_line__
+
+import os
+
+class View:
+
+    def cabecera():
+        print(__header_W__)
+        print(__header_PMC__)
+        print(__header_line__)
+        print()
+
+    def clear():
+        # Para windows
+        if os.name == 'nt':
+            _ = os.system('cls')
+        # Para mac y linux(here, os.name is 'posix')
+        else:
+            _ = os.system('clear')
+
+
